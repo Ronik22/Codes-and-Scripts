7057ae79484a38fa2311a8b162bdef592e7a2b0d
Ronik22 <ronikbhattacharjee22112000@gmail.com>
2021-03-26 00:39:15 +0530

    Create speech_translator.py

---
+++ speech_translator.py
@@ -0,0 +1,161 @@
+# pip install googletrans==3.1.0a0
+
+import speech_recognition as sr 
+from gtts import gTTS
+from googletrans import Translator
+import os
+
+r = sr.Recognizer() 
+
+def SpeakText(command,langinp): 
+    tts = gTTS(text=command, lang=langinp)
+    tts.save("translatedvoice.mp3")
+    os.system("translatedvoice.mp3")
+
+def speech_to_text():
+
+    print("\nSay something...")	 
+    try: 	
+        with sr.Microphone() as source2: 
+            r.adjust_for_ambient_noise(source2, duration=0.2) 
+            audio2 = r.listen(source2) 
+                
+            MyText = r.recognize_google(audio2) 
+            MyText = MyText.lower() 
+
+            print("You said: "+MyText) 
+            texttranslate(MyText)
+                
+    except sr.RequestError as e: 
+        print("Could not request results; {0}".format(e)) 
+            
+    except sr.UnknownValueError: 
+        print("unknown error occured") 
+
+
+def texttranslate(inp):
+    translator = Translator(service_urls=['translate.googleapis.com'])
+    print("""
+        -----------------
+        Code: Language
+        -----------------
+        'af': 'afrikaans',
+        'sq': 'albanian',
+        'am': 'amharic',
+        'ar': 'arabic',
+        'hy': 'armenian',
+        'az': 'azerbaijani',
+        'eu': 'basque',
+        'be': 'belarusian',
+        'bn': 'bengali',
+        'bs': 'bosnian',
+        'bg': 'bulgarian',
+        'ca': 'catalan',
+        'ceb': 'cebuano',
+        'ny': 'chichewa',
+        'zh-cn': 'chinese (simplified)',
+        'zh-tw': 'chinese (traditional)',
+        'co': 'corsican',
+        'hr': 'croatian',
+        'cs': 'czech',
+        'da': 'danish',
+        'nl': 'dutch',
+        'en': 'english',
+        'eo': 'esperanto',
+        'et': 'estonian',
+        'tl': 'filipino',
+        'fi': 'finnish',
+        'fr': 'french',
+        'fy': 'frisian',
+        'gl': 'galician',
+        'ka': 'georgian',
+        'de': 'german',
+        'el': 'greek',
+        'gu': 'gujarati',
+        'ht': 'haitian creole',
+        'ha': 'hausa',
+        'haw': 'hawaiian',
+        'iw': 'hebrew',
+        'he': 'hebrew',
+        'hi': 'hindi',
+        'hmn': 'hmong',
+        'hu': 'hungarian',
+        'is': 'icelandic',
+        'ig': 'igbo',
+        'id': 'indonesian',
+        'ga': 'irish',
+        'it': 'italian',
+        'ja': 'japanese',
+        'jw': 'javanese',
+        'kn': 'kannada',
+        'kk': 'kazakh',
+        'km': 'khmer',
+        'ko': 'korean',
+        'ku': 'kurdish (kurmanji)',
+        'ky': 'kyrgyz',
+        'lo': 'lao',
+        'la': 'latin',
+        'lv': 'latvian',
+        'lt': 'lithuanian',
+        'lb': 'luxembourgish',
+        'mk': 'macedonian',
+        'mg': 'malagasy',
+        'ms': 'malay',
+        'ml': 'malayalam',
+        'mt': 'maltese',
+        'mi': 'maori',
+        'mr': 'marathi',
+        'mn': 'mongolian',
+        'my': 'myanmar (burmese)',
+        'ne': 'nepali',
+        'no': 'norwegian',
+        'or': 'odia',
+        'ps': 'pashto',
+        'fa': 'persian',
+        'pl': 'polish',
+        'pt': 'portuguese',
+        'pa': 'punjabi',
+        'ro': 'romanian',
+        'ru': 'russian',
+        'sm': 'samoan',
+        'gd': 'scots gaelic',
+        'sr': 'serbian',
+        'st': 'sesotho',
+        'sn': 'shona',
+        'sd': 'sindhi',
+        'si': 'sinhala',
+        'sk': 'slovak',
+        'sl': 'slovenian',
+        'so': 'somali',
+        'es': 'spanish',
+        'su': 'sundanese',
+        'sw': 'swahili',
+        'sv': 'swedish',
+        'tg': 'tajik',
+        'ta': 'tamil',
+        'te': 'telugu',
+        'th': 'thai',
+        'tr': 'turkish',
+        'uk': 'ukrainian',
+        'ur': 'urdu',
+        'ug': 'uyghur',
+        'uz': 'uzbek',
+        'vi': 'vietnamese',
+        'cy': 'welsh',
+        'xh': 'xhosa',
+        'yi': 'yiddish',
+        'yo': 'yoruba',
+        'zu': 'zulu',
+
+    """)
+
+    langinp=input("Enter code for translated language: ")
+    translated = translator.translate(inp, langinp)
+
+    print("Translated Text: ",translated.text)
+    print("Pronounciation: ",translated.pronunciation)
+    SpeakText(str(translated.text),langinp)
+
+
+if __name__ == '__main__':
+    speech_to_text()
